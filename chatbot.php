f35465671666e0889352c353afc4362d17283490
C:\Users\Administrator\Desktop\pybotbot <kanyamat003@gmail.com>
2017-11-19 15:37:57 +0700

    t

--- chatbot.php
+++ chatbot.php
@@ -1346,6 +1346,46 @@ $q = pg_exec($dbconn, "UPDATE users_register SET hospital_number = $answer WHERE
 
 #########################################################################################################################################################
 
+}elseif (strpos($_msg) !== false && $seqcode == "0027" ) {
+               $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
+                while ($row = pg_fetch_row($result)) {
+                  echo $answer = $row[0]; 
+                }   
+            $u = pg_escape_string($_msg); 
+            $replyToken = $event['replyToken'];
+
+        $messages = [
+          'type'=> 'template',
+          'altText'=> 'this is a buttons template',
+          'template'=> [
+              'type'=> 'buttons',
+              //'thumbnailImageUrl'=> 'https://example.com/bot/images/image.jpg',
+              'title'=> "ช่วงระหว่างการตั้งครรภ์คุณออกกำลังกายในระดับไหน",
+              'text'=> "......",
+              'actions'=> [
+                  [
+                    'type'=> 'message',
+                    'label'=> 'เบา',
+                    'text'=> 'เบา'
+                  ],
+                  [
+                    'type'=> 'message',
+                    'label'=> 'ปานกลาง',
+                    'text'=> 'ปานกลาง'
+                  ],
+                  [
+                    'type'=> 'message',
+                    'label'=> 'หนัก',
+                    'text'=> 'หนัก'
+                  ]
+              ]
+          ]
+        ];
+
+$q = pg_exec($dbconn, "UPDATE users_register SET  history_food = '{$u}' WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
+$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0027','{$u}','1001','0',NOW(),NOW())") or die(pg_errormessage());
+
+#########################################################################################################################################################
 }elseif ($event['message']['text'] == "แพ้ยา"  ) {
                $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
                 while ($row = pg_fetch_row($result)) {
@@ -1431,46 +1471,7 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
 
 ########################################################################################################################################################
 
-}elseif (strpos($_msg) !== false && $seqcode == "0027" ) {
-               $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
-                while ($row = pg_fetch_row($result)) {
-                  echo $answer = $row[0]; 
-                }   
-            $u = pg_escape_string($_msg); 
-            $replyToken = $event['replyToken'];
 
-        $messages = [
-          'type'=> 'template',
-          'altText'=> 'this is a buttons template',
-          'template'=> [
-              'type'=> 'buttons',
-              //'thumbnailImageUrl'=> 'https://example.com/bot/images/image.jpg',
-              'title'=> "ช่วงระหว่างการตั้งครรภ์คุณออกกำลังกายในระดับไหน",
-              'text'=> "......",
-              'actions'=> [
-                  [
-                    'type'=> 'message',
-                    'label'=> 'เบา',
-                    'text'=> 'เบา'
-                  ],
-                  [
-                    'type'=> 'message',
-                    'label'=> 'ปานกลาง',
-                    'text'=> 'ปานกลาง'
-                  ],
-                  [
-                    'type'=> 'message',
-                    'label'=> 'หนัก',
-                    'text'=> 'หนัก'
-                  ]
-              ]
-          ]
-        ];
-
-$q = pg_exec($dbconn, "UPDATE users_register SET  history_food = '{$u}' WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
-$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0027','{$u}','1001','0',NOW(),NOW())") or die(pg_errormessage());
-
-#########################################################################################################################################################
 
 
   }else {
