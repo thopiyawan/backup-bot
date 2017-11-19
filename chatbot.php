9157c1b5a8200bb350684a3900e0d3d86799ad8c
C:\Users\Administrator\Desktop\pybotbot <kanyamat003@gmail.com>
2017-11-19 22:23:22 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -422,22 +422,22 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
 
 ########################################################################################################################################################
 
- }elseif ($event['message']['text'] == "กำหนดการคลอด" ) {
-               $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
-                while ($row = pg_fetch_row($result)) {
-                  echo $answer = $row[0]; 
-                }   
+// }elseif ($event['message']['text'] == "กำหนดการคลอด" ) {
+//                $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
+//                 while ($row = pg_fetch_row($result)) {
+//                   echo $answer = $row[0]; 
+//                 }   
 
-                 $u = pg_escape_string($answer);
+//                  $u = pg_escape_string($answer);
                  
-                 $replyToken = $event['replyToken'];
-                 $messages = [
-                        'type' => 'text',
-                        'text' => 'ขอทราบกำหนดการคลอดของคุณหน่อยค่ะ (กรุณาตอบวันที่และเดือนเป็นตัวเลขนะคะ เช่น 17 04 คือ วันที่ 17 เมษายน)'
-                      ];
+//                  $replyToken = $event['replyToken'];
+//                  $messages = [
+//                         'type' => 'text',
+//                         'text' => 'ขอทราบกำหนดการคลอดของคุณหน่อยค่ะ (กรุณาตอบวันที่และเดือนเป็นตัวเลขนะคะ เช่น 17 04 คือ วันที่ 17 เมษายน)'
+//                       ];
 
- $q = pg_exec($dbconn, "UPDATE users_register SET user_weight = $answer WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
-$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','2015','','0016','0',NOW(),NOW())") or die(pg_errormessage()); 
+//  $q = pg_exec($dbconn, "UPDATE users_register SET user_weight = $answer WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
+// $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','2015','','0016','0',NOW(),NOW())") or die(pg_errormessage()); 
 ########################################################################################################################################################
 
  }elseif (strlen($_msg) == 5 && $seqcode == "1015") {
@@ -544,104 +544,104 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
 
 
 // ########################################################################################################################################################
- }elseif (strlen($_msg) == 5 && $seqcode == "2015") {
-     $pieces = explode(" ", $_msg);
-    $date = str_replace("","",$pieces[0]);
-    $month  = str_replace("","",$pieces[1]);
+ // }elseif (strlen($_msg) == 5 && $seqcode == "2015") {
+ //     $pieces = explode(" ", $_msg);
+ //    $date = str_replace("","",$pieces[0]);
+ //    $month  = str_replace("","",$pieces[1]);
    
-            $today_years= date("Y") ;
-            $today_month= date("m") ;
-            $today_day  = date("d") ;
+ //            $today_years= date("Y") ;
+ //            $today_month= date("m") ;
+ //            $today_day  = date("d") ;
           
-            if(($month>$today_month&& $month<=12 && $date<=31) || ($month==$today_month && $date>$today_day)  ){
-                $years = $today_years-1;
-                $strDate1 = $years."-".$month."-".$date;
-                $strDate2=date("Y-m-d");
+ //            if(($month>$today_month&& $month<=12 && $date<=31) || ($month==$today_month && $date>$today_day)  ){
+ //                $years = $today_years-1;
+ //                $strDate1 = $years."-".$month."-".$date;
+ //                $strDate2=date("Y-m-d");
                 
-                $date_pre =  (strtotime($strDate2) - strtotime($strDate1))/( 60 * 60 * 24 );
-                $week = $date_pre/7;
-                $week_preg = number_format($week);
-                $day = $date_pre%7;
-                $day_preg = number_format($day);
-                $age_pre = 'คุณมีอายุครรภ์'. $week_preg .'สัปดาห์'.  $day_preg .'วัน' ;
-                      $replyToken = $event['replyToken'];
-                      $messages = [
-                          'type' => 'template',
-                          'altText' => 'this is a confirm template',
-                          'template' => [
-                              'type' => 'confirm',
-                              'text' =>  $age_pre.'ใช่ไหมคะ?' ,
-                              'actions' => [
-                                  [
-                                      'type' => 'message',
-                                      'label' => 'ใช่',
-                                      'text' => 'อายุครรภ์ถูกต้อง'
-                                  ],
-                                  [
-                                      'type' => 'message',
-                                      'label' => 'ไม่ใช่',
-                                      'text' => 'ไม่ถูกต้อง'
-                                  ],
-                              ]
-                          ]
-                      ];   
+ //                $date_pre =  (strtotime($strDate2) - strtotime($strDate1))/( 60 * 60 * 24 );
+ //                $week = $date_pre/7;
+ //                $week_preg = number_format($week);
+ //                $day = $date_pre%7;
+ //                $day_preg = number_format($day);
+ //                $age_pre = 'คุณมีอายุครรภ์'. $week_preg .'สัปดาห์'.  $day_preg .'วัน' ;
+ //                      $replyToken = $event['replyToken'];
+ //                      $messages = [
+ //                          'type' => 'template',
+ //                          'altText' => 'this is a confirm template',
+ //                          'template' => [
+ //                              'type' => 'confirm',
+ //                              'text' =>  $age_pre.'ใช่ไหมคะ?' ,
+ //                              'actions' => [
+ //                                  [
+ //                                      'type' => 'message',
+ //                                      'label' => 'ใช่',
+ //                                      'text' => 'อายุครรภ์ถูกต้อง'
+ //                                  ],
+ //                                  [
+ //                                      'type' => 'message',
+ //                                      'label' => 'ไม่ใช่',
+ //                                      'text' => 'ไม่ถูกต้อง'
+ //                                  ],
+ //                              ]
+ //                          ]
+ //                      ];   
             
-            }elseif($month<$today_month && $month<=12 && $date<=31){
-                $strDate1 = $today_years."-".$month."-".$date;
-                $strDate2=date("Y-m-d");
-                $date_pre =  (strtotime($strDate2) - strtotime($strDate1))/( 60 * 60 * 24 );;
-                $week = $date_pre/7;
-                $week_preg = number_format($week);
-                $day = $date_pre%7;
-                $day_preg = number_format($day);
-                $age_pre = 'คุณมีอายุครรภ์'. $week_preg .'สัปดาห์'.  $day_preg .'วัน' ;
-                    $replyToken = $event['replyToken'];
-                    $messages = [
-                        'type' => 'template',
-                        'altText' => 'this is a confirm template',
-                        'template' => [
-                            'type' => 'confirm',
-                            'text' =>  $age_pre.'ใช่ไหมคะ?' ,
-                            'actions' => [
-                                [
-                                    'type' => 'message',
-                                    'label' => 'ใช่',
-                                    'text' => 'อายุครรภ์ถูกต้อง'
-                                ],
-                                [
-                                    'type' => 'message',
-                                    'label' => 'ไม่ใช่',
-                                    'text' => 'ไม่ถูกต้อง'
-                                ],
-                            ]
-                        ]
-                    ];   
-            }else{
-               $replyToken = $event['replyToken'];
-                 $messages = [
-                        'type' => 'text',
-                        'text' => 'ดูเหมือนคุณจะพิมพ์ไม่ถูกต้อง'
-                      ];
-            }
+ //            }elseif($month<$today_month && $month<=12 && $date<=31){
+ //                $strDate1 = $today_years."-".$month."-".$date;
+ //                $strDate2=date("Y-m-d");
+ //                $date_pre =  (strtotime($strDate2) - strtotime($strDate1))/( 60 * 60 * 24 );;
+ //                $week = $date_pre/7;
+ //                $week_preg = number_format($week);
+ //                $day = $date_pre%7;
+ //                $day_preg = number_format($day);
+ //                $age_pre = 'คุณมีอายุครรภ์'. $week_preg .'สัปดาห์'.  $day_preg .'วัน' ;
+ //                    $replyToken = $event['replyToken'];
+ //                    $messages = [
+ //                        'type' => 'template',
+ //                        'altText' => 'this is a confirm template',
+ //                        'template' => [
+ //                            'type' => 'confirm',
+ //                            'text' =>  $age_pre.'ใช่ไหมคะ?' ,
+ //                            'actions' => [
+ //                                [
+ //                                    'type' => 'message',
+ //                                    'label' => 'ใช่',
+ //                                    'text' => 'อายุครรภ์ถูกต้อง'
+ //                                ],
+ //                                [
+ //                                    'type' => 'message',
+ //                                    'label' => 'ไม่ใช่',
+ //                                    'text' => 'ไม่ถูกต้อง'
+ //                                ],
+ //                            ]
+ //                        ]
+ //                    ];   
+ //            }else{
+ //               $replyToken = $event['replyToken'];
+ //                 $messages = [
+ //                        'type' => 'text',
+ //                        'text' => 'ดูเหมือนคุณจะพิมพ์ไม่ถูกต้อง'
+ //                      ];
+ //            }
   
-      $url = 'https://api.line.me/v2/bot/message/reply';
-         $data = [
-          'replyToken' => $replyToken,
-          'messages' => [$messages],
-         ];
-         error_log(json_encode($data));
-         $post = json_encode($data);
-         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-         $ch = curl_init($url);
-         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-         $result = curl_exec($ch);
-         curl_close($ch);
-         echo $result . "\r\n";
-    $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','2015', $week_preg ,'0017','0',NOW(),NOW())") or die(pg_errormessage());
+ //      $url = 'https://api.line.me/v2/bot/message/reply';
+ //         $data = [
+ //          'replyToken' => $replyToken,
+ //          'messages' => [$messages],
+ //         ];
+ //         error_log(json_encode($data));
+ //         $post = json_encode($data);
+ //         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+ //         $ch = curl_init($url);
+ //         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ //         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ //         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+ //         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ //         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ //         $result = curl_exec($ch);
+ //         curl_close($ch);
+ //         echo $result . "\r\n";
+ //    $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','2015', $week_preg ,'0017','0',NOW(),NOW())") or die(pg_errormessage());
 
 
 
