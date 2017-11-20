94dece0e94c1a3e06beb403f74290b2c5e0ca994
C:\Users\Administrator\Desktop\pybotbot <kanyamat003@gmail.com>
2017-11-20 21:35:23 +0700

    a

--- chatbot.php
+++ chatbot.php
@@ -413,7 +413,7 @@ $q = pg_exec($dbconn, "UPDATE users_register SET user_weight = $answer WHERE use
                         'text' => 'ขอทราบครั้งสุดท้ายที่คุณมีประจำเดือนเพื่อคำนวณอายุครรภ์ค่ะ (กรุณาตอบวันที่และเดือนเป็นตัวเลขนะคะ เช่น 17 04 คือ วันที่ 17 เมษายน)'
                       ];
 
-$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','1015','','0017','0',NOW(),NOW())") or die(pg_errormessage());
+$q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','1015','','0016','0',NOW(),NOW())") or die(pg_errormessage());
 
  
 ########################################################################################################################################################
