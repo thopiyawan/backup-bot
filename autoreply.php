caa1fa140fe990a5f67d7fbdb26cc9b9ba54ec75
kanyamat <kanyamat003@gmail.com>
2017-11-23 10:00:30 +0700

    1

--- autoreply.php
+++ autoreply.php
@@ -67,7 +67,7 @@ for($x = 0; $x <= $arrlength ; $x++) {
                                 'type' => 'text',
                                 'text' => 'สัปดาห์นี้คุณแม่มีน้ำหนักเท่าไรแล้วคะ?'
                              ];
-                $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0013','','0000','0',NOW(),NOW())") or die(pg_errormessage()); 
+                $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','1003','','0000','0',NOW(),NOW())") or die(pg_errormessage()); 
                 // $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight,updated_at )VALUES('{$user_id}',$p_week,'0',  NOW()) ") or die(pg_errormessage());        
                      
                  $url = 'https://api.line.me/v2/bot/message/push';
